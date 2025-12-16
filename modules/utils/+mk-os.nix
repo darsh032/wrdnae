a32b2871ca27e6544809f8c05c19f86315adc1b0
Darsh yadav <darsh.yadav@proton.me>
2025-12-16 11:16:22 +0530

    yoinked by argosnothing

---
+++ modules/utils/+mk-os.nix
@@ -0,0 +1,42 @@
+# Honestly just yoinked from @argosnothing
+
+{
+  inputs,
+  config,
+  lib,
+  ...
+}: let
+  flake.lib.mk-os = {
+    inherit mkNixos;
+    inherit linux;
+  };
+
+  linux = mkNixos "x86_64-linux";
+
+  mkNixos = system: name:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        config.flake.modules.nixos.${name}
+        {
+          my.hostname = name;
+          networking.hostName = lib.mkDefault name;
+          nixpkgs = {
+            hostPlatform = lib.mkDefault system;
+            overlays = [
+              (_: super: {
+                inherit (super.stdenv.hostPlatform) system;
+              })
+            ];
+            config = {
+              allowUnfree = true;
+              showAliases = true;
+            };
+          };
+          system.stateVersion = "25.05";
+        }
+      ];
+    };
+in {
+  inherit flake;
+}
