e4b388deb014cdfe0e9e5dd47447b1b92db3c7f3
Darsh yadav <darsh.yadav@proton.me>
2025-12-17 00:12:40 +0530

    smooth

--- modules/packages/neovim.nix
+++ modules/packages/neovim.nix
@@ -3,14 +3,12 @@
     packages.neovim = (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
       
-      modules = [
-        {
-          config.vim = {
-            theme.enable = true;
-            treesitter.enable = true;
-          };
-        }
-      ];
+      modules = [{
+        config.vim = {
+          theme.enable = true;
+          treesitter.enable = true;
+        };
+      }];
     }).neovim;
   };
 }
