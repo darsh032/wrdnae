1675f3171e9c2494e8605bbe6ff2565e226e1bcb
Darsh yadav <darsh.yadav@proton.me>
2025-12-26 20:25:02 +0530

    OH

--- modules/packages/nvf/nvf.nix
+++ modules/packages/nvf/nvf.nix
@@ -18,19 +18,6 @@ in {
     mkNvf = modules:
       (inputs.nvf.lib.neovimConfiguration {
         inherit pkgs modules;
-
-            config.vim = {
-      lsp = {
-        enable = true;
-        trouble.enable = true;
-      };
-
-      diagnostics = {
-        enable = true;
-        nvim-lint.enable = true;
-      };
-    };
-
       }).neovim;
   in {
     packages = {
