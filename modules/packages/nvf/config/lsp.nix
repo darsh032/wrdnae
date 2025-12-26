209fb6dc51c83da399e0242d8436b07fd6c240fc
Darsh yadav <darsh.yadav@proton.me>
2025-12-26 20:16:16 +0530

    yeah

---
+++ modules/packages/nvf/config/lsp.nix
@@ -0,0 +1,12 @@
+{
+  flake.modules.nvf.lsp = {
+    vim = {
+      lsp = {
+        enable = true;
+        trouble.enable = true;
+      }
+
+      diagnostics.enable = true;
+    }
+  };
+}
--- modules/packages/nvf/config/vim.nix
+++ modules/packages/nvf/config/vim.nix
@@ -4,13 +4,10 @@
       theme.enable = true;
       treesitter.enable = true;
       git.enable = true;
+      options.shiftwidth = 4;
       clipboard = {
         enable = true;
         registers = "unnamedplus";
-      };
-      lsp = {
-        enable = true;
-        trouble.enable = true;
       };
     };
   };
