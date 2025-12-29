ff3f3631e20f99495d88d93fdee38c8e053ce225
Darsh yadav <darsh.yadav@proton.me>
2025-12-29 19:53:35 +0530

    helix: theme change

--- modules/packages/helix/_base.nix
+++ modules/packages/helix/_base.nix
@@ -1,5 +1,5 @@
 {
-  theme = "tokyonight";
+  theme = "ashen";
 
   editor = {
     soft-wrap.enable = true;
@@ -9,13 +9,13 @@
     bufferline = "multiple";
 
     lsp.display-messages = true;
-
-    # statusline = {
-    #   mode.normal = "NORMAL";
-    #   mode.insert = "INSERT";
-    #   mode.select = "SELECT";
+  
+    statusline = {
+      mode.normal = "NORMAL";
+      mode.insert = "INSERT";
+      mode.select = "SELECT";
     #   right = [ "file-type" "position" ];
-    # };
+    };
     # color-modes = true;
 
     cursor-shape = {
